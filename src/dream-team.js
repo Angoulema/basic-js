d1cd3b05c8cd7bc3f2fdf10948e23744f1994a8d
angoulema <eugenia.labokha@yandex.by>
2020-10-02 22:35:40 +0300

    feat: add half-solution for DreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+//  throw new CustomError('Not implemented');
+  if (Array.isArray(members)) {
+    let str = "";
+    let arr = members;
+    arr = arr.sort();
+    arr.forEach(item => str = str + item[0]);
+    return str.toUpperCase();
+/*arr.reduce( (acc, item) =>{
+      if (typeof item === 'string') {
+        acc = acc + item[0];
+      } else return false;
+    }, "")
+    return acc.toString().toUpperCase(); */
+  } else return false;
 };
+
+// не работает с пробелами, не игнорирует не строки и нестандартные случаи
