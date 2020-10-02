09630c687e592e96f4c92bfc2b96b4d95c32ed61
angoulema <eugenia.labokha@yandex.by>
2020-10-02 21:29:18 +0300

    feat: add solution to Counting Cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+//  throw new CustomError('Not implemented');
+  let array = matrix.reduce(
+    (result, item) => result.concat(item), [] // выносит все вложенные массивы на уровень выше
+  );
+  let sum = 0;
+  array.forEach( item => {  // проверяю наличие котиков, если котики есть, считаю их
+    if (item === "^^") {
+      sum = sum + 1;
+    };
+  });
+  return sum;
 };
