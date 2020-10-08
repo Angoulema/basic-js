53bc6c7732d6aa054a33e1f7d7c217a6a85124e9
angoulema <eugenia.labokha@yandex.by>
2020-10-08 22:35:46 +0300

    feat: add solution to seasons

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+//  throw new CustomError('Not implemented');
+
+if (!date) {
+  return 'Unable to determine the time of year!'; // если не дата
+};
+
+if (!Date.prototype.toString.call(date)) { //если скрытая не дата
+  throw new Error();
+};
+
+let month = date.getMonth();
+let theMonth; // тот самый искомый месяц в строковом виде
+switch (month) {
+  case 2:
+  case 3:
+  case 4:
+    theMonth = 'spring';
+    break;
+  case 5:
+  case 6:
+  case 7: 
+    theMonth = 'summer';
+    break;
+  case 8:
+  case 9:
+   case 10:
+    theMonth = 'autumn';
+    break;
+  default: theMonth = 'winter';
+    break;
+};
+return theMonth;
 };
