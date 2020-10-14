9dcac791513ef5dbfa808130493e6d8aa25915d8
angoulema <eugenia.labokha@yandex.by>
2020-10-14 23:39:01 +0300

    feat: add solution to repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+//  throw new CustomError('Not implemented');
+  let separator = String(options.separator);
+if (separator == "" || separator == "undefined") {
+  separator = '+';
+ };
+
+ let separatorAdd = String(options.additionSeparator)
+ if (separatorAdd == "" || separatorAdd == "undefined") {
+  separatorAdd = '|';
+ };
+
+ let strAdded = ""; // доп. строка
+ let addition = String(options.addition);
+ if (addition !== "" && addition !== "undefined") { //если его передали
+  strAdded = addition; // если кол-во повторов - 1
+  for (let i = 2; i <= options.additionRepeatTimes; i++) {
+    strAdded = strAdded + separatorAdd + String(options.addition);
+  };
+ };
+
+ let strPlus = String(str) + strAdded; // повторяющаяся часть
+ let strNew = String(str) + strAdded; // если кол-во повторов - 1
+ for (let i = 2; i <= options.repeatTimes; i++) { // если кол-во повторов не задано, итоговая строка равна поданной
+  strNew = strNew + separator + strPlus; 
+ };
+
+
+ return strNew;
+
+
 };
   
+// options[repeatTimes]
+// options[separator]
+// options[addition]
+// options[additionRepeatTimes]
+// options[additionSeparator]
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-/* if (Array.isArray(arr) && arr.length) {
-let minusNext = "--discard-next";
-let minusPrev = "--discard-prev";
-let doubleNext = '--double-next';
-let doublePrev = '--double-prev';
-let indexMinusNext = arr.indexOf(minusNext);
-let indexMinusPrev = arr.indexOf(minusPrev);
-let indexDoubleNext = arr.indexOf(doubleNext);
-let indexDoublePrev = arr.indexOf(doublePrev);
-let newArr = [];
-
-
-
-} else throw new Error();
-*/
+//  throw new CustomError('Not implemented');
+// тры тэсты падаюць
+if (Array.isArray(arr) && arr.length) {
+  
+     let arrTransformed = [];
+     for (let i = 0; i < arr.length; i++) {
+       if (arr[i] === "--discard-next") {
+         if (arr[i + 1]) {
+          i++;
+         }
+         
+         
+       } else if (arr[i] === "--discard-prev") {
+         if (arr[i - 1] === arrTransformed[i - 1]) {
+           arrTransformed = arrTransformed.slice(0, i - 1);
+           
+         }
+         
+       } else if (arr[i] === "--double-next") {
+        if (arr[i + 1]) {
+          arrTransformed = arrTransformed.concat(arr[i + 1]);
+        }
+        
+       }
+       else if (arr[i] === "--double-prev") {
+         if (arr[i - 1] === arrTransformed[i - 1]) {
+           arrTransformed = arrTransformed.concat(arr[i - 1]);
+          
+         }
+         
+       } else {
+         arrTransformed = arrTransformed.concat(arr[i]);
+       };
+       
+     };
+ return arrTransformed;
+ 
+ } else if (Array.isArray(arr) && arr.length === 0) {
+  let arrTransformed = [];
+  return arrTransformed;
+ } else {
+    throw new Error();
+   };
+   
 };
