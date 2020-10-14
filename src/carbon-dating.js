3620150bf00b510cadb70d74474182c62edb83b0
angoulema <eugenia.labokha@yandex.by>
2020-10-14 19:56:43 +0300

    feat: add solution to carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,22 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //  throw new CustomError('Not implemented');
+  
+  if (!sampleActivity || (typeof sampleActivity !== "string") || !sampleActivity.match(/^[\d+]$/g)) {
+    console.log(sampleActivity, "false");
+    return false;
+  } else if ((+sampleActivity >= 15) || (+sampleActivity <= 0)) {
+    console.log(sampleActivity, "false");
+    return false;
+  } else 
+     {
+    let k = Math.LN2 / HALF_LIFE_PERIOD;
+    let t_years = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
+    let t_whole = Math.ceil(t_years);
+  
+    console.log(t_whole);
+    return t_whole;
+  };
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+/* if (Array.isArray(arr) && arr.length) {
+let minusNext = "--discard-next";
+let minusPrev = "--discard-prev";
+let doubleNext = '--double-next';
+let doublePrev = '--double-prev';
+let indexMinusNext = arr.indexOf(minusNext);
+let indexMinusPrev = arr.indexOf(minusPrev);
+let indexDoubleNext = arr.indexOf(doubleNext);
+let indexDoublePrev = arr.indexOf(doublePrev);
+let newArr = [];
+
+
+
+} else throw new Error();
+*/
 };
