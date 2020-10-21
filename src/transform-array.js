57c5665482e0a0f7d57f2cfadd24f2594c4e9e3e
angoulema <eugenia.labokha@yandex.by>
2020-10-21 23:24:04 +0300

    fix: fix solution almost

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,37 +2,37 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
 //  throw new CustomError('Not implemented');
-// тры тэсты падаюць
+// адзін тэст падае
 if (Array.isArray(arr) && arr.length) {
   
      let arrTransformed = [];
      for (let i = 0; i < arr.length; i++) {
        if (arr[i] === "--discard-next") {
          if (arr[i + 1]) {
-          i++;
+            i++;    
          }
          
          
        } else if (arr[i] === "--discard-prev") {
-         if (arr[i - 1] === arrTransformed[i - 1]) {
-           arrTransformed = arrTransformed.slice(0, i - 1);
+         if (arr[i - 1] === arrTransformed[i - 1] && arrTransformed[i - 1]) {
+           arrTransformed.pop();
            
          }
          
        } else if (arr[i] === "--double-next") {
         if (arr[i + 1]) {
-          arrTransformed = arrTransformed.concat(arr[i + 1]);
+          arrTransformed.push(arr[i + 1]);
         }
         
        }
        else if (arr[i] === "--double-prev") {
-         if (arr[i - 1] === arrTransformed[i - 1]) {
-           arrTransformed = arrTransformed.concat(arr[i - 1]);
+         if ((arr[i - 1] === arrTransformed[i - 1]) && arrTransformed[i - 1]) {
+           arrTransformed.push(arr[i - 1]);
           
          }
          
        } else {
-         arrTransformed = arrTransformed.concat(arr[i]);
+         arrTransformed.push(arr[i]);
        };
        
      };
